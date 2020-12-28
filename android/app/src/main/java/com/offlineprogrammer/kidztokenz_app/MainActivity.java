90d034e140fc3a8203432df365e4c78404f611a8
offlineProgrammer <mmalaka@gmail.com>
2020-12-27 21:52:26 -0800

    Initial commit

---
+++ android/app/src/main/java/com/offlineprogrammer/kidztokenz_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.offlineprogrammer.kidztokenz_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,52 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "analytics": {
+        "plugins": {
+            "awsPinpointAnalyticsPlugin": {
+                "pinpointAnalytics": {
+                    "appId": "cf697512f010434794c8e6c3950609c1",
+                    "region": "us-east-1"
+                },
+                "pinpointTargeting": {
+                    "region": "us-east-1"
+                }
+            }
+        }
+    },
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:6308d12f-e9de-4189-ae1b-fe11c654717e",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                },
+                "PinpointAnalytics": {
+                    "Default": {
+                        "AppId": "cf697512f010434794c8e6c3950609c1",
+                        "Region": "us-east-1"
+                    }
+                },
+                "PinpointTargeting": {
+                    "Default": {
+                        "Region": "us-east-1"
+                    }
+                }
+            }
+        }
+    }
+}''';
