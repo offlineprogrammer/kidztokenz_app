5d593930bd1100ed31cb8167dfcb433e31a63105
offlineProgrammer <mmalaka@gmail.com>
2020-12-28 16:10:54 -0800

    Signup Form

--- lib/main.dart
+++ lib/main.dart
@@ -17,24 +17,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kidz Tokenz',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Kidz Tokenz Home Page'),
     );
   }
 }
@@ -92,23 +80,26 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              height: 200,
-              child: Image.asset(
-                'assets/images/storeicon.png',
-                fit: BoxFit.cover,
+      body: Container(
+        color: Color(0xff90CCE6),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: Color(0xff90CCE6),
+                height: 200,
+                child: Image.asset(
+                  'assets/images/storeicon.png',
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SignUpView(),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SignUpView(),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/sign_up.dart
+++ lib/widgets/sign_up.dart
@@ -1,3 +1,5 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_core/amplify_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -11,10 +13,64 @@ class _SignUpViewState extends State<SignUpView> {
   final passwordController = TextEditingController();
   DateTime signupDate;
 
+  String _signUpError = "";
+  List<String> _signUpExceptions = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _signIn() async {
+    // Sign out before in case a user is already signed in
+    // If a user is already signed in - Amplify.Auth.signIn will throw an exception
+    try {
+      await Amplify.Auth.signOut();
+    } on AuthError catch (e) {
+      print(e);
+    }
+
+    try {
+      SignInResult res = await Amplify.Auth.signIn(
+          username: emailController.text.trim(),
+          password: passwordController.text.trim());
+      Navigator.pop(context, true);
+    } on AuthError catch (e) {
+      setState(() {
+        _signUpError = e.cause;
+        _signUpExceptions.clear();
+        e.exceptionList.forEach((el) {
+          _signUpExceptions.add(el.exception);
+        });
+      });
+    }
+  }
+
+  void _signUp() async {
+    try {
+      Map<String, dynamic> userAttributes = {
+        "email": emailController.text.trim(),
+        // additional attributes as needed
+      };
+      SignUpResult res = await Amplify.Auth.signUp(
+          username: emailController.text.trim(),
+          password: passwordController.text.trim(),
+          options: CognitoSignUpOptions(userAttributes: userAttributes));
+
+      print(res.isSignUpComplete);
+
+      //  setState(() {
+      // isSignUpComplete = res.isSignUpComplete;
+      //  });
+    } on AuthError catch (e) {
+      print(e.toString);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 5,
+      elevation: 2,
       child: Container(
         padding: EdgeInsets.all(5),
         child: Column(
@@ -24,16 +80,24 @@ class _SignUpViewState extends State<SignUpView> {
               controller: emailController,
             ),
             TextField(
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
               decoration: InputDecoration(labelText: 'Password'),
               controller: passwordController,
             ),
             FlatButton(
-              textColor: Theme.of(context).primaryColor,
-              onPressed: () {},
+              textColor: Colors.black, // Theme.of(context).primaryColor,
+              color: Colors.amber,
+              onPressed: _signUp,
               child: Text(
                 'Create Account',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
+            ),
+            Text(
+              'Already registered? Sign In',
+              style: Theme.of(context).textTheme.subtitle1,
             ),
           ],
         ),
